b24c0302757f5721cf327dba2caf9687fbff89b0
Megan Moore <meganmoore.dev@gmail.com>
2023-08-25 14:41:14 -0400

    Add project files.

---
+++ FramWorkExamples/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FramWorkExamples
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        
+        public AppDbContext() { } //default constructor
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connStr = "server=localhost\\sqlexpress;" + //connection string
+                            "database=SalesDb;" +
+                            "trusted_connection=true;" +
+                            "trustServerCertificate=true;";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+    }
+}
---
+++ FramWorkExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using FramWorkExamples;
+
+
+var _context = new AppDbContext();
+var customers = from c in _context.Customers  //query syntax
+                select c;
+
+foreach(var cust in customers)
+{
+    Console.WriteLine(cust);
+}
